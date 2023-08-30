ee4dc33483d4b6c7131b638dfbf185c88beabdfd
Zeal5 <tassadiqshakeel5@gmail.com>
2023-08-30 13:27:17 +0500

    swap tokens backend complete

--- robin_backend/apis/routers/buy_tokens.py
+++ robin_backend/apis/routers/buy_tokens.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 weth_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 dai_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-
+usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 class TokenForSwap(BaseModel):
     token_address:str = None
     tg_id:int
@@ -28,23 +28,30 @@ async def swap_tokens(data:TokenForSwap):
     # key = wallet[0]
     # print(key.address)
     address = Token(buyer_address="0x136be469A3203D20a853a546Af89867AE4B437b9",
-                    token_address=weth_address,
-                    buyer_secret="0xc55c8750c851c723e36485856c96760f33efb7b3b281e9a0ecf674f3070f0939"
-                    
+                    buyer_secret="0xc55c8750c851c723e36485856c96760f33efb7b3b281e9a0ecf674f3070f0939",
+                    token_to_sell=dai_address,
+                    token_to_buy=usdt,
                     )
+    
     # balance = await address.swap_tokens_for_eth(1731.17)
 
-    balance = await address.swap_tokens_for_eth(1700)
+    balance = await address.swap_tokens_for_tokens(100)
 
     print(balance)
     # return (balance)
 
 @router.post("/get_token_balance")
-async def get_token_balance(data:TokenForSwap) ->float:
+async def get_token_balance(data:TokenForSwap):
     # token = Token(data.token_address)
+    
     address = Token(buyer_address="0x136be469A3203D20a853a546Af89867AE4B437b9",
-                    token_address=weth_address
-                    )
+                    token_address_for_info=weth_address)
     balance = await address.get_token_balance()
     print(f"token balance : {format_number(balance)}")
     return f"{format_number(balance)}"
+    
+    # usdt = Token(token_address_for_info="0x6B175474E89094C44Da98b954EedeAC495271d0F")
+    # decimal = await usdt._get_decimals()
+
+    # print(decimal)
+    # return decimal
--- robin_backend/on_chain/buy.py
+++ robin_backend/on_chain/buy.py
@@ -1,6 +1,6 @@
 import asyncio
 from web3 import Web3
-from . import token_abi, uniswap_router_abi
+from . import token_abi as ERC20_token_abi, uniswap_router_abi
 import time
 
 import json
@@ -19,103 +19,128 @@ class Token:
 
     def __init__(
         self,
-        token_address: str = None,
+        token_address_for_info: str = None,
         buyer_secret: str = None,
         buyer_address: str = None,
+        token_to_buy: str = None,
+        token_to_sell: str = None,
     ) -> None:
         self.web3 = Web3(Web3.HTTPProvider(Token.web3_provider_url))
-        print(f"is connected : {self.web3.is_connected()}")
+        # print(f"is connected : {self.web3.is_connected()}")
         self.buyer_address = buyer_address
-        self.token_address = token_address
+        self.token_address_for_info = token_address_for_info
         self.buyer_secret = buyer_secret
+        self.token_to_buy = token_to_buy
+        self.token_to_sell = token_to_sell
 
-        if self.buyer_address is not None and not self.web3.is_checksum_address(
-            self.buyer_address
+        if buyer_address is not None and not self.web3.is_checksum_address(
+            buyer_address
         ):
-            self.buyer_address = self.web3.to_checksum_address(self.buyer_address)
+            self.buyer_address = self.web3.to_checksum_address(buyer_address)
 
-        if self.token_address is not None and not self.web3.is_checksum_address(
-            self.token_address
+        if token_address_for_info is not None and not self.web3.is_checksum_address(
+            token_address_for_info
         ):
-            self.token_address = self.web3.to_checksum_address(self.token_address)
+            self.token_address_for_info = self.web3.to_checksum_address(
+                token_address_for_info
+            )
+
+        if token_to_buy is not None and not self.web3.is_checksum_address(token_to_buy):
+            self.token_to_buy = self.web3.to_checksum_address(token_to_buy)
+
+        if token_to_sell is not None and not self.web3.is_checksum_address(
+            token_to_sell
+        ):
+            self.token_to_sell = self.web3.to_checksum_address(token_to_sell)
 
         # @TODO this is set to default account for testing only
         self.account = self.web3.eth.default_account = self.buyer_address
 
-    async def _get_decimals(self) -> int:
+    #@TODO update decimals and other info related to token to db(create a class lvl dict to hold that info?) to reduce the calls made to rpc
+    async def _get_decimals(self, _token: str = None) -> int:
         """Get decimals of a contract"""
-        loop = asyncio.get_event_loop()
-        contract = self.web3.eth.contract(address=self.token_address, abi=token_abi)
-        decimals = await loop.run_in_executor(
-            None, lambda: contract.functions.decimals().call()
+
+        if _token is None:
+            _token = self.token_address_for_info
+
+        print(f"token = {_token}")
+        print(f"is check_sum {self.web3.is_checksum_address(_token)}")
+        contract = self.web3.eth.contract(
+            address=_token, abi=ERC20_token_abi
         )
+        decimals = contract.functions.decimals().call()
+        print(f"decimals {decimals}")
+
         return decimals
 
-    async def get_token_balance(self) -> int:
+    async def get_token_balance(self, _token: str = None) -> int | float:
         """Return token balance after dividing by token decimals"""
-        loop = asyncio.get_event_loop()
+        if _token is None:
+            _token = self.token_address_for_info
 
-        contract = self.web3.eth.contract(address=self.token_address, abi=token_abi)
-        token_balance = await loop.run_in_executor(
-            None, lambda: contract.functions.balanceOf(self.buyer_address).call()
-        )
-        decimals = await self._get_decimals()
-        token_balance = token_balance / (10**decimals)
+        contract = self.web3.eth.contract(address=_token, abi=ERC20_token_abi)
+        token_balance = contract.functions.balanceOf(self.buyer_address).call()
+
+        decimals = await self._get_decimals(_token=_token)
+        token_balance = token_balance / 10**decimals
 
         return token_balance
 
     async def _get_eth_balance(self) -> int:
         eth_balance = self.web3.eth.get_balance(self.buyer_address)
         return self.web3.from_wei(eth_balance, "ether")
-    
 
-    #@TODO only approves tokens for router and use _get_decimal automatically
-    async def _approve_tokens(self, amount_to_approve):
-        token_contract = self.web3.eth.contract(dai_address, abi=token_abi)
-        print('token_contract created succesfully')
+    # @TODO only approves tokens for router and use _get_decimal automatically
+    async def _approve_tokens(
+        self, amount_to_approve: int, _token_to_approve: str = None
+    ):
+        if _token_to_approve is None:
+            _token_to_approve = self.token_address_for_info
+
+        token_contract = self.web3.eth.contract(_token_to_approve, abi=ERC20_token_abi)
+        print("token_contract created succesfully")
         approve_txn = token_contract.functions.approve(
-            router_address, amount_to_approve *10**18
+            router_address,
+            amount_to_approve
+            * 10 ** (await self._get_decimals(_token=_token_to_approve)),
         ).build_transaction(
             {
                 "from": self.buyer_address,
                 "gasPrice": self.web3.eth.gas_price,
-                "nonce": self.web3.eth.get_transaction_count(self.buyer_address)
+                "nonce": self.web3.eth.get_transaction_count(self.buyer_address),
             }
         )
         print(approve_txn)
-        signed_approve_txn = self.web3.eth.account.sign_transaction(approve_txn, private_key=self.buyer_secret)
+        signed_approve_txn = self.web3.eth.account.sign_transaction(
+            approve_txn, private_key=self.buyer_secret
+        )
 
         self.web3.eth.send_raw_transaction(signed_approve_txn.rawTransaction)
         return approve_txn
 
-    async def swap_tokens_for_eth(self, token_amount_to_spend: int):
+    async def swap_tokens_for_tokens(self, token_amount_to_spend: int):
         # @TODO get token decimals using _get_decimals method
-        amount = await self._get_eth_balance()
-        if amount > 0:
-            print(f"amount {amount}")
-            await self._approve_tokens(token_amount_to_spend)
+        if 3 > 0:
+            await self._approve_tokens(
+                amount_to_approve=token_amount_to_spend,_token_to_approve = self.token_to_sell
+            )
+
+            path = [self.token_to_sell, self.token_to_buy]
             nonce = self.web3.eth.get_transaction_count(self.buyer_address)
             contract = self.web3.eth.contract(router_address, abi=uniswap_router_abi)
-            weth = contract.functions.WETH().call()
-            money = token_amount_to_spend * 10**18
-            print(money)
-            print(self.token_address)
+            money = token_amount_to_spend * 10** (await self._get_decimals(_token = self.token_to_sell))
+            print(f"path {path}")
             amount_out = contract.functions.getAmountsOut(
-                int(money), [dai_address, self.token_address]
+                int(money), path
             ).call()
-            print(f"amount out dai -> weth {amount_out[0] / (10**18)} - {amount_out[1] / (10**18)}")
+            print(amount_out)
+            print(f"amount out dai -> usdt {amount_out[0] / (10**(await self._get_decimals(_token = path[0])))} - {amount_out[1] / (10**(await self._get_decimals(_token = path[1])))}")
             print('args')
-            print(token_amount_to_spend * 10**18,
-                int(0.96 *10**18),
-                [dai_address, self.token_address],
-                self.buyer_address,
-                int(time.time() + 10))
-
             # @TODO set custom slipage
             buy_token = contract.functions.swapExactTokensForTokens(
-                token_amount_to_spend * 10**18,
-                int(0.96 *10**18),
-                [dai_address, self.token_address],
+                token_amount_to_spend * 10**(await self._get_decimals(_token = self.token_to_sell)),
+                int(96 *10**(await self._get_decimals(_token = self.token_to_buy))),
+                path,
                 self.buyer_address,
                 int(time.time() + 1000)
             ).build_transaction(
